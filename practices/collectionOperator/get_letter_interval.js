f07d84725c9a850ed42efa1d757cc0a5ae43bda7
fanfan123 <1820148513@qq.com>
2018-03-30 15:32:28 +0800

    test

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,19 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var letter=[];
+  for (var i=0; i<26;i++){
+    letter.push(String.fromCharCode((97+i)))
+  }
+  var result=[];
+  if(number_a<number_b){
+    result = letter.slice(number_a-1,number_b)
+  }
+  else if(number_a>number_b){
+    result = letter.slice(number_b-1,number_a);
+    result.reverse();
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
