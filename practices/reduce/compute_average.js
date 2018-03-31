048585d85fb9536eaeb6e87d14286a5a37c05210
fanfan123 <1820148513@qq.com>
2018-04-01 00:02:14 +0800

    reduce

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,13 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var sum=0;
+  function getSum (item, index, array) {
+    sum += item;
+  }
+  collection.forEach(getSum);
+  return sum/collection.length;
+
 }
 
 module.exports = compute_average;
