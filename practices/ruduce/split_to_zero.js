d6f8a9700a64dae119d84fbe7bb73be7b99312fe
fanfan123 <1820148513@qq.com>
2018-04-04 14:39:52 +0800

    reduce2 complete

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,15 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result=[];
+  for(var i=0;i<1000;i++){
+    result.push(number);
+    if(number <= 0){
+      break;
+    }
+    number = Number((number-interval).toFixed(1))
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
