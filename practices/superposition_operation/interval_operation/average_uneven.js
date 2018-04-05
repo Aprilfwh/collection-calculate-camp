f9442b5bbfd18871394ae031d5970342478dbfde
fanfan123 <1820148513@qq.com>
2018-04-05 16:36:52 +0800

    interval operation complete

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,15 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  var num =0;
+  collection.forEach(function (value) {
+    if(value%2!==0){
+      sum += value;
+      num += 1;
+    }
+  });
+  return sum/num;
 }
 
 module.exports = average_uneven;
--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -3,6 +3,34 @@
 function median_to_letter(collection) {
 
   //在这里写入代码
+  var median;
+  var col = collection.sort(function (a,b) {
+    return a-b;
+  });
+  if(col.length%2!==0){
+    median=col[(col.length-1)/2];
+  }
+  else {
+    median=Math.ceil((col[col.length/2]+col[col.length/2-1])/2);
+  }
+  var letter=[String.fromCharCode((97+25))];
+  for (var i=0; i<25;i++){
+    letter.push(String.fromCharCode((97+i)))
+  }
+  var consult1 = Math.floor(median/26);
+  var mod1 = median%26;
+  if(consult1===0 ){
+    return letter[mod1];
+  }
+  else if(consult1===1 && mod1===0){
+    return letter[mod1];
+  }
+  else if(consult1>1 && mod1===0){
+    return letter[consult1-1]+letter[mod1];
+  }
+  else{
+    return letter[consult1]+letter[mod1];
+  }
 }
 
 module.exports = median_to_letter;
