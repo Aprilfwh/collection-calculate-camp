9a3c50d9d9e50913e47bba50cc1575c141d0aba1
fanfan123 <1820148513@qq.com>
2018-04-05 17:28:14 +0800

    own elements operation

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,25 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+  var result;
+  var even =[];
+  var uneven=[];
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]%2===0){
+      even.push(collection[i]);
+    }
+    else {
+      uneven.push(collection[i]);
+    }
+  }
+  function AscSort(a,b) {
+    return a-b;
+  }
+  function DescSort(a,b) {
+    return b-a;
+  }
+  even.sort(AscSort);
+  uneven = uneven.sort(DescSort);
+  result = even.concat(uneven);
+  return result;
 };
 module.exports = even_asc_odd_desc;
--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,30 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
   //这里写代码。。。
+  var result=[];
+  var arr = collection.sort(function (a, b) {
+    return a-b;
+  });
+  var temp1 =[];
+  var temp2 =[];
+  for (var i=0; i<arr.length;i++){
+    if (i%3===0 && temp1.length < Math.floor(arr.length/3)){
+      temp1.push(arr[i]);
+    }
+    else{
+      temp2.push(arr[i]);
+    }
+  }
+  for(var j=0;j<temp2.length;j++){
+    var len_result = result.length;
+    if((len_result+1)%3===0){
+      result.push(temp1[(len_result+1)/3-1]);
+      result.push(temp2[j]);
+    }
+    else {
+      result.push(temp2[j]);
+    }
+  }
+  return result;
 }
 module.exports = rank_by_two_large_one_small;
