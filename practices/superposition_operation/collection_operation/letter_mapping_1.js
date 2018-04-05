daf795fc27f5c4356aa802222ce3be2ad758d8da
fanfan123 <1820148513@qq.com>
2018-04-05 16:19:59 +0800

    collection operation complete

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -3,6 +3,17 @@
 function even_to_letter(collection) {
 
   //在这里写入代码
+  var result=[];
+  var letter=[String.fromCharCode((97+25))];
+  for (var i=0; i<25;i++){
+    letter.push(String.fromCharCode((97+i)))
+  }
+  for(var j=0;j<collection.length;j++){
+    if(collection[j]%2===0){
+      result.push(letter[collection[j]%26])
+    }
+  }
+  return result;
 }
 
 module.exports = even_to_letter;
--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -3,6 +3,15 @@
 function average_to_letter(collection) {
 
   //在这里写入代码
+  var average;
+  var sum =0;
+  collection.forEach(function (value) {
+    sum += value;
+  });
+  average = Math.ceil(sum/collection.length);
+  var index = (average-1) %26;
+  return String.fromCharCode((97+index));
+
 }
 
 module.exports = average_to_letter;
